a4a7d89a47f5400679edf57d64d970cc4ab6eef6
“Alexandrie” <alexandrie.be@gmail.com>
2022-01-06 17:57:46 +0100

    Add dynamic Head to dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 // thedolcevita.com/
+import Head from "next/head";
 import Home from "../components/Home/Home";
 
 function HomePage() {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>The Dolce Vita Restaurant</title>
+        <meta
+          name="description"
+          content="The Dolce Vita Restaurant - Menu and Reservation"
+        />
+      </Head>
+      <Home />
+    </>
+  );
 }
 
 export default HomePage;
--- pages/menu/[category].js
+++ pages/menu/[category].js
@@ -1,8 +1,46 @@
 // thedolcevita.com/menu/category
 import Content from "../../components/Content/Content";
+import Head from "next/head";
 
-function Category() {
-  return <Content />;
+function Category({ category }) {
+  return (
+    <>
+      <Head>
+        <title>Menu {category}</title>
+        <meta name="description" content={`Menu ${category}`} />
+      </Head>
+      <Content />;
+    </>
+  );
+}
+
+export function getStaticProps({ params = {} } = {}) {
+  return {
+    props: {
+      category: params.category,
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = [
+    "starters",
+    "salads",
+    "main",
+    "desserts",
+    "brunch",
+    "cocktails",
+  ].map((item) => {
+    return {
+      params: {
+        category: item,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default Category;
